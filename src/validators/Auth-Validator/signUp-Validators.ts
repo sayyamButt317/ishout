2ab4773afad8ac216a2861b127d14c0506e0ffb5
affantechtimize <affan.techtimize@gmail.com>
2026-02-19 14:53:28 +0500

    added country phone picker  dropdown

--- src/validators/Auth-Validator/signUp-Validators.ts
+++ src/validators/Auth-Validator/signUp-Validators.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
+import { isValidPhoneNumber } from 'react-phone-number-input';
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const phoneRegex = /^\+?[1-9]\d{1,14}$/;
 
 export const SignUpFormSchema = z
     .object({
@@ -9,7 +9,12 @@ export const SignUpFormSchema = z
             .string()
             .min(3, { message: 'Company name must be at least 3 characters long.' }),
         contact_person: z.string().min(2, { message: 'Contact person must be at least 2 characters long.' }),
-        phone: z.string().regex(phoneRegex, { message: 'Please enter a valid phone number.' }),
+        phone: z
+            .string()
+            .min(1, { message: 'Please enter your phone number.' })
+            .refine((value) => isValidPhoneNumber(value), {
+                message: 'Please enter a valid phone number.',
+            }),
         // industry: z.string().min(1, { message: 'Industry type is required.' }),
         // company_size: z.string().min(1, { message: 'Company size is required.' }),
         email: z.string().regex(emailRegex, { message: 'Please enter a valid email address.' }),
