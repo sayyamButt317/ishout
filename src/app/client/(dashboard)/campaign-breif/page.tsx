8585349c15f55255c5dba225a935801dea036cd8
sayyamButt317 <sayyambutt317@gmail.com>
2026-02-20 11:13:11 +0500

    add use client

--- src/app/client/(dashboard)/campaign-breif/page.tsx
+++ src/app/client/(dashboard)/campaign-breif/page.tsx
@@ -1,8 +1,9 @@
+'use client';
 import { Input } from '@/components/ui/input';
 import Button from '@/src/app/component/button';
 import CampaignBreifHook from '@/src/routes/Admin/Hooks/CampaignBreif-hook';
 import { Loader2 } from 'lucide-react';
-import React, { useState } from 'react'
+import { useState } from 'react';
 
 const CampaignBreifPage = () => {
     const { mutate: generateCampaignBreif, isPending } = CampaignBreifHook();
