1949098098e0886a618e3e2772c230725c2c08ef
sayyamButt317 <sayyambutt317@gmail.com>
2026-02-22 03:20:18 +0500

    about us text

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -4,6 +4,11 @@ const AboutUs = () => {
     return (
         <div>
             <h1>The Next Era of Influencer Marketing is Intelligent.</h1>
+            <p>iShout Media empowers brands to run high performing influencer campaigns seamlessly, at scale.
+
+                Our AI driven platform handles discovery, workflow, and reporting,
+
+                so you can focus on impact.</p>
         </div>
     )
 }
