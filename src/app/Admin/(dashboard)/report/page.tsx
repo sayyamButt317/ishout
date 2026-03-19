43a82fc37d34fb132c8ed963ad3b3f21b429f623
sayyamButt317 <sayyambutt317@gmail.com>
2026-03-19 12:22:34 +0500

    report generation

--- src/app/Admin/(dashboard)/report/page.tsx
+++ src/app/Admin/(dashboard)/report/page.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function ContentReportGeneration() {
   return (
     <div>ContentReportGeneration</div>
---
+++ src/app/client/(dashboard)/reports/page.tsx
@@ -0,0 +1,5 @@
+export default function BrandReportGeneration() {
+    return (
+      <div>ContentReportGeneration</div>
+    )
+  }
