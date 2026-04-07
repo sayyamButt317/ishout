b112d1840e030d7b1a8829d7842a71e28acf1094
talhasaleem77 <talhasaleem.techtimize@gmail.com>
2026-04-07 16:21:13 +0500

    Feature/updated_ui

--- src/app/client/(dashboard)/approved-contents/all-campaigns/page.tsx
+++ src/app/client/(dashboard)/approved-contents/all-campaigns/page.tsx
@@ -72,7 +72,7 @@ export default function ClientInfluencersContentPage() {
                   );
                 }}
               >
-                View
+                View Feedback
               </Button>
             </div>,
           ];
